144ab78ea8f4c4f16df68b4abce6edebe127efa1
lordszn3 <robertkuzhin@gmail.com>
2022-11-01 20:46:52 +0500

    fix: dynamic import

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import solid from '@astrojs/solid-js'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
-export const site = (username) => `https://${username}.github.io/raiden.me/`
+export const site = (username) => `https://${username}.github.io`
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   site: site('raidenmiro'),
+  base: '/raiden.me',
   markdown: {
     shikiConfig: {
       theme: 'rose-pine-moon',
